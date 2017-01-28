8de9e8f45fbd126010293cc13befa444a42e64c2
Gregory <grihabor@mail.ru>
2017-01-28 21:33:32 +0300

    cell structure, model constructors

--- model.py
+++ model.py
@@ -1,4 +1,26 @@
 
 
+class Cell:
+    def __init__(self, left, top, right, bottom, active=False):
+        self.left = left
+        self.right = right
+        self.top = top
+        self.bottom = bottom
+        self.active = active
+
+
 class Model:
-    pass
+    def __init__(self, cell_list):
+        self.cell_list = cell_list
+
+    @classmethod
+    def from_matrix(cls, matrix):
+        pass
+
+    @classmethod
+    def from_textfile(cls, filename):
+        pass
+
+    @classmethod
+    def from_image(cls, filename):
+        pass
