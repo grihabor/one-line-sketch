17bbd3ebad994f9df4d3da6d670600ccd1de0ea6
Gregory <grihabor@mail.ru>
2017-01-29 01:56:09 +0300

    improved dfs

--- sketch_model.py
+++ sketch_model.py
@@ -1,5 +1,8 @@
+from threading import Thread
+
 import networkx as nx
 import matplotlib.pyplot as plt
+import pylab
 
 class Cell:
     def __init__(self, left, top, right, bottom, status=0):
@@ -11,16 +14,53 @@ class Cell:
 
 
 class SketchModel:
-    def __init__(self, graph, start_node=None, width=None, height=None):
+    def __init__(self, graph : nx.Graph, start_node=None, width=None, height=None):
         self.graph = graph
+        self.figure_index = None
         if width and height:
             self.width = width
             self.height = height
             self.start_node = start_node
 
     def solve(self):
-        node = self.graph.node[self.start_node]
-        print(node)
+        cur_node = self.start_node
+        self.node_count = 1
+        self.dup_graph = self.graph.copy()
+
+        def dfs(cur_node):
+            if self.node_count == self.graph.number_of_nodes():
+                return True
+
+            self.dup_graph.remove_node(cur_node)
+
+            number_connected_components = nx.number_connected_components(self.dup_graph)
+
+            leaves_count = sum(1 for node in self.dup_graph.nodes_iter()
+                               if self.dup_graph.degree(node) == 1)
+            print(number_connected_components, leaves_count)
+
+            self.draw(True)
+
+            if number_connected_components == 1 and leaves_count <= 2:
+                for neighbor_node in self.graph.neighbors_iter(cur_node):
+                    if self.graph.node[neighbor_node]['color'] == 'b':
+                        self.graph.node[neighbor_node]['color'] = 'r'
+                        self.node_count += 1
+                        if dfs(neighbor_node):
+                            return True
+
+            self.graph.node[cur_node]['color'] = 'b'
+            self.node_count -= 1
+
+            # TODO: fix this line!!! (add not all edges, but only with existing nodes)
+            self.dup_graph.add_edges_from(self.graph.edges([cur_node]))
+
+            self.dup_graph.node[cur_node]['color'] = 'b'
+            return False
+
+        print(dfs(cur_node))
+        self.draw()
+
 
     @classmethod
     def from_matrix(cls, matrix):
@@ -46,16 +86,30 @@ class SketchModel:
 
         return SketchModel(grid_graph, start_node, width, height)
 
-    @property
-    def _pos(self):
+    def _pos(self, graph):
         return {node: (node[1], self.height-1-node[0])
-                for node in self.graph.nodes()}
+                for node in graph.nodes()}
 
-    def draw(self, filename=None):
+    def draw(self, dup=False, filename=None):
 
-        node_color = [node[1]['color'] for node in self.graph.nodes(data=True)]
+        def node_color(graph):
+            return [node[1]['color'] if 'color' in node[1] else 'b'
+                    for node in graph.nodes(data=True)]
 
-        nx.draw(self.graph, self._pos, node_color=node_color)
+        if self.figure_index:
+            self.figure_index += 1
+        else:
+            self.figure_index = 1
+
+        if dup:
+            plt.subplot(121)
+        nx.draw(self.graph, self._pos(self.graph),
+                node_color=node_color(self.graph))
+        if dup:
+            plt.subplot(122)
+            nx.draw(self.dup_graph, self._pos(self.dup_graph),
+                    node_color=node_color(self.dup_graph))
+
         if filename:
             plt.savefig(filename)
         else:
@@ -72,3 +126,4 @@ class SketchModel:
     @classmethod
     def from_image(cls, filename):
         pass
+
