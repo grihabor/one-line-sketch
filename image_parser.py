a444bfaa600c82a418885ba5fa8e7eff2e97cea4
Gregory <grihabor@mail.ru>
2017-02-04 13:43:28 +0300

    trying sobel

--- image_parser.py
+++ image_parser.py
@@ -1,6 +1,11 @@
+import scipy
+from skimage.color import rgb2gray
+from skimage.filters import sobel
+from skimage.filters import threshold_adaptive
+from skimage.filters import threshold_otsu
 from skimage.io import imread
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 class ImageParser:
     def __init__(self):
@@ -8,7 +13,22 @@ class ImageParser:
 
     def parse(self, filename):
         img = imread(filename)
+        img = rgb2gray(img)
+        #print(img.shape)
+        #img = threshold_adaptive(img, 51)
+        img = sobel(img)
+        row_sum = np.sum(img, axis=1)
+        print(len(row_sum), row_sum)
+
+        plt.subplot(121)
+        plt.plot(row_sum)
+
+
+
+        plt.subplot(122)
         plt.imshow(img)
+        print(img)
+        plt.show()
 
     @property
     def matrix(self):
