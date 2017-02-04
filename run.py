7e7444ba2867d89f41911e176dea6d316cbc8f26
Gregory <grihabor@mail.ru>
2017-02-04 11:51:16 +0300

    reorganize data folder

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from sketch_model import SketchModel
 
+DATA_PATH = 'data'
+TXT_DATA_PATH = DATA_PATH + '/txt'
+IMG_DATA_PATH = DATA_PATH + '/img'
+
 
 def main():
-    model = SketchModel.from_textfile('data/expert/rose.txt')
+    model = SketchModel.from_textfile(IMG_DATA_PATH + '/expert/rose.txt')
     #model.draw()
     model.solve()
 
